417df9dcd679a1a4ac2d7393c00234c6fc780769
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-10-11 22:27:22 +0700

    init v2 project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,5 @@
-import '../assets/css/global.css'
-
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '@/views/HomePage.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,8 +7,16 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomePage,
-    }
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
   ],
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  server: {
-    host: true
-  },
   plugins: [
     vue(),
     vueDevTools(),
