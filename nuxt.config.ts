5292be1755f4f168563a2b8588c8b64d1b15ba2b
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-10-11 23:30:14 +0700

    feat: add tailwind css for styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -9,5 +11,13 @@ export default defineNuxtConfig({
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/scripts'
-  ]
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
 })
