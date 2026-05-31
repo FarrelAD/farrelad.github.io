022d5d8d91fec70f43dd1a7caaee3e584f24d5b3
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2026-05-31 22:43:44 +0700

    refactor: change nuxt to astro fully working web

---
+++ src/i18n/utils.ts
@@ -0,0 +1,14 @@
+import en from './locales/en.json';
+import id from './locales/id.json';
+
+const translations = { en, id };
+
+export type Locale = 'en' | 'id';
+
+export function useTranslations(locale: Locale) {
+  const dict = translations[locale] || en;
+  return function t(key: string): string {
+    const value = key.split('.').reduce((obj: any, keyPart) => obj?.[keyPart], dict);
+    return typeof value === 'string' ? value : key;
+  };
+}
